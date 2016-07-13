000ab153cbd696e5c93fac1b959ca7f1e49ebe87
ArsenArsen <arsen.arsenovic@hotmail.rs>
2016-07-13 20:51:58 +0200

    Fix a silly NPE
    Signed-off-by: ArsenArsen <arsen.arsenovic@hotmail.rs>

--- src/com/bwfcwalshy/jarchecker/Logger.java
+++ src/com/bwfcwalshy/jarchecker/Logger.java
@@ -9,30 +9,30 @@ public class Logger {
 	
 	public static void print(String msg){
 		String x = getTime() + " [INFO] " + msg;
-		Main.mw.log.append(x + "\n");
+		if(!Main.nogui) Main.mw.log.append(x + "\n");
 		System.out.println(x);
 	}
 	
 	public static void printNoInfo(String msg) {
 		System.out.println(msg);
-		Main.mw.log.append(msg + "\n");
+		if(!Main.nogui) Main.mw.log.append(msg + "\n");
 	}
 	
 	public static void debug(String msg){
 		String x = getTime() + " [DEBUG] "+ msg;
-		Main.mw.log.append(x + "\n");
+		if(!Main.nogui) Main.mw.log.append(x + "\n");
 		System.out.println(x);
 	}
 	
 	public static void error(String msg){
 		String x = getTime() + " [ERROR] " + msg;
-		Main.mw.log.append(x + "\n");
+		if(!Main.nogui) Main.mw.log.append(x + "\n");
 		System.err.println(x);
 	}
 	
 	public static void emptyLine() {
 		System.out.println();
-		Main.mw.log.append("\n");
+		if(!Main.nogui) Main.mw.log.append("\n");
 	}
 	
 	public static String getTime(){
--- src/com/bwfcwalshy/jarchecker/Main.java
+++ src/com/bwfcwalshy/jarchecker/Main.java
@@ -10,56 +10,60 @@ import com.bwfcwalshy.jarchecker.gui.MainWindow;
 
 public class Main {
 
-	private static final String VERSION = "v0.6";
-	public static MainWindow mw;
-	
-	public static void main(String[] args) throws ZipException, IOException {
-		if(args.length == 1) {
-			String path;
-			Scanner scanner = null;
-			if(args[0] == "nogui") {
-				scanner = new Scanner(System.in);
-				path = scanner.next();
-			} else path = args[0];
+    private static final String VERSION = "v0.6";
+    public static MainWindow mw;
+    public static boolean nogui = false;
 
-			decompilerStart(path);
-			
-			if(scanner != null) scanner.close();
-		} else {
-			// GUI goes here
-			MainWindow mw = new MainWindow();
-			mw.setVisible(true);
-			Main.mw = mw;
-		}
-	}
-	
-	public static String getVersion() {
-		return VERSION;
-	}
+    public static void main(String[] args) throws ZipException, IOException {
+	if(args.length == 1) {
+	    nogui = true;
+	    String path;
+	    Scanner scanner = null;
+	    if(args[0].equalsIgnoreCase("nogui")) {
+		scanner = new Scanner(System.in);
+		path = scanner.next();
+	    } else path = args[0];
 
-	public static Map<String, String> decompilerStart(String path) {
-		Decompiler decompiler = new Decompiler();
-		File f = new File(path);
-		File export = new File(f.getName().replace(".jar", "") + "-src");
 
-		Logger.print("Decompiling file.");
-		boolean success = decompiler.decompile(f, export);
-		Checker checker = new Checker();
-		if(success){
-			Logger.print("Decompiled jar file!");
-			checker.check(new File(export.getAbsolutePath() + File.separator + f.getName()));
-		}else{
-			Logger.error("Unable to decompile jar file!!");
-			System.exit(1);
-		}
-		Logger.print("-----------------------------------------------------");
-		Logger.printNoInfo("Jar name: " + f.getName());
-		Logger.emptyLine();
-		Logger.printNoInfo("File checked with JarChecker " + VERSION + " by bwfcwalshy");
-		Logger.emptyLine();
-		Logger.printNoInfo("Found: " + (checker.getFound().isEmpty() ? "Nothing!" : "\n" + checker.getFound()));
-		Logger.printNoInfo("Plugin is " + checker.getWarningLevel() + "!");
-		
-		return checker.getSuspiciusClasses();
+	    decompilerStart(path);
+
+
+	    if(scanner != null) scanner.close();
+	} else {
+	    // GUI goes here
+	    MainWindow mw = new MainWindow();
+	    mw.setVisible(true);
+	    Main.mw = mw;
 	}
+    }
+
+    public static String getVersion() {
+	return VERSION;
+    }
+
+    public static Map<String, String> decompilerStart(String path) {
+	Decompiler decompiler = new Decompiler();
+	File f = new File(path);
+	File export = new File(f.getName().replace(".jar", "") + "-src");
+
+	Logger.print("Decompiling file.");
+	boolean success = decompiler.decompile(f, export);
+	Checker checker = new Checker();
+	if(success){
+	    Logger.print("Decompiled jar file!");
+	    checker.check(new File(export.getAbsolutePath() + File.separator + f.getName()));
+	}else{
+	    Logger.error("Unable to decompile jar file!!");
+	    System.exit(1);
+	}
+	Logger.print("-----------------------------------------------------");
+	Logger.printNoInfo("Jar name: " + f.getName());
+	Logger.emptyLine();
+	Logger.printNoInfo("File checked with JarChecker " + VERSION + " by bwfcwalshy");
+	Logger.emptyLine();
+	Logger.printNoInfo("Found: " + (checker.getFound().isEmpty() ? "Nothing!" : "\n" + checker.getFound()));
+	Logger.printNoInfo("Plugin is " + checker.getWarningLevel() + "!");
+
+	return checker.getSuspiciusClasses();
+    }
 }
