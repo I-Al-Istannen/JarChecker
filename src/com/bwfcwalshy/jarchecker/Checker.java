c53f42d849ac4bab6ce3a5eadfd57e7bb5004587
ArsenArsen <arsen.arsenovic@hotmail.rs>
2016-07-14 17:31:13 +0200

    Add improvements by I Al Istannen
    Signed-off-by: ArsenArsen <arsen.arsenovic@hotmail.rs>

--- src/com/bwfcwalshy/jarchecker/Checker.java
+++ src/com/bwfcwalshy/jarchecker/Checker.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.regex.Matcher;
+import java.util.function.Predicate;
 import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
@@ -37,8 +37,8 @@ public class Checker {
 	    try {
 		String className = jar.getName().substring(0, jar.getName().lastIndexOf('.'));
 		boolean suspicious = false;
-	    	String clazz = "";
-	    	int ln = 0;
+		String clazz = "";
+		int ln = 0;
 		for(String s : Files.readAllLines(jar.toPath())) {
 		    Map<Checks, Integer> result = checkLine(s, ++ln, className);
 		    clazz += "\n";
@@ -48,14 +48,14 @@ public class Checker {
 			int rc = 0;
 			for(Entry<Checks, Integer> c : result.entrySet()) {
 			    rc++;
-			    clazz += formatName(c.getKey());
+			    clazz += c.getKey().toString();
 			    if(rc < result.size()) clazz += ", ";
 			}
 		    }
 		    clazz += "\n";
 		}
 		if(suspicious) {
-			foundClasses.put(className, clazz);
+		    foundClasses.put(className, clazz);
 		}
 	    } catch (IOException e) {
 		Logger.error(e);
@@ -84,7 +84,7 @@ public class Checker {
 				int lc = 0;
 				for(Entry<Checks, Integer> c : result.entrySet()) {
 				    lc++;
-				    clazz += formatName(c.getKey());
+				    clazz += c.getKey().toString();
 				    if(lc < result.size()) clazz += ", ";
 				}
 			    }
@@ -109,26 +109,18 @@ public class Checker {
 		    if(zip != null)
 			zip.close();
 		}catch(IOException e){
-			Logger.error(e);
+		    Logger.error(e);
 		}
 	    }
     }
 
-    public Map<Checks, Integer> checkLine(String line, int ln, String className){
+    // Function improved upon a request by I Al Istannen
+    public Map<Checks, Integer> checkLine(String line, int ln, String className) {
 	Map<Checks, Integer> founds = new HashMap<>();
-	for(Checks check : Checks.values()){
-	    if(!check.isPattern()){
-		if(line.contains(check.getString())) {
-		    found(check, ln, line, className);
-		    founds.put(check, founds.containsKey(check) ? founds.get(check) + 1 : 0);
-		}
-	    }else{
-		Matcher matcher = check.getPattern().matcher(line);
-		boolean found = matcher.find();
-		if(found) {
-		    found(check, ln, line, className);
-		    founds.put(check, founds.containsKey(check) ? founds.get(check) + 1 : 0);
-		}
+	for (Checks check : Checks.values()) {
+	    if(check.matches(line)) {
+		found(check, ln, line, className);
+		founds.put(check, founds.containsKey(check) ? founds.get(check) + 1 : 1);
 	    }
 	}
 
@@ -175,31 +167,28 @@ public class Checker {
     public int getWarningCount() {
 	return warningCount;
     }
-    
+
     public String getFound() {
 	StringBuilder sb = new StringBuilder();
 	Map<Checks, Integer> count = new HashMap<>();
 	for(Checks check : foundChecks){
-	    count.put(check, count.containsKey(check) ? count.get(check)+1 : 0);
+	    count.put(check, count.containsKey(check) ? count.get(check)+1 : 1);
 	}
 	int rc = 0;
 	for(Entry<Checks, Integer> e : count.entrySet()) {
 	    rc++;
-	    sb.append(formatName(e.getKey()));
+	    sb.append(e.getKey().toString());
 	    sb.append(" x"+e.getValue());
 	    if(rc < count.size()) sb.append("\n");
 	}
 	return sb.toString();
     }
 
-    private String formatName(Checks e) {
-	return e.toString().charAt(0) + e.toString().replace("_", " ").substring(1).toLowerCase();
-    }
-
     public Map<String, String> getSuspiciusClasses(){
 	return foundClasses;
     }
 
+    // Enum improved upon a request by I Al Istannen
     enum Checks {
 	THREAD_SLEEP("Thread.sleep", WarningType.MALICIOUS),
 	WHILE_TRUE(Pattern.compile("while\\((\\s*)?true(\\s*)?\\)"), WarningType.MALICIOUS),
@@ -207,45 +196,39 @@ public class Checker {
 	ENDLESS_LOOP(Pattern.compile("for\\((\\s*)?;(\\s*)?;(\\s*)?;(\\s*)?\\)"), WarningType.MALICIOUS),
 	SET_OP(Pattern.compile("setOp\\((\\s*)?true(\\s*)?\\)"), WarningType.WARNING),
 	EQUALS_NAME(Pattern.compile("getName\\(\\).(equals|equalsIgnoreCase)\\(\\\"[a-zA-Z0-9]+\\\"\\)"), WarningType.WARNING),
-	STAR_PERM("addPermission(\"*\"", WarningType.WARNING),
+	STAR_PERM(Pattern.compile("addPermission\\((\\s*)?\"\\*\""), WarningType.WARNING),
 	URL(Pattern.compile("(https?):\\/\\/(www.)?[a-zA-Z]+.[a-zA-Z]+.([a-zA-Z]+)?"), WarningType.WARNING),
 	IP_ADDRESS(Pattern.compile("\\d{1,3}.+\\:?\\d{1,5}$"), WarningType.WARNING),
-	//We can't really make this more accurate.
+	// We can't really make this more accurate.
 	OP_ME("opme", WarningType.MALICIOUS),
 	EXIT(".exit(", WarningType.MALICIOUS);
 
-	private String s;
-	private Pattern p;
+	private Predicate<String> predicate;
 	private WarningType type;
 
-	private Checks(String s, WarningType type){
-	    this.s = s;
-	    this.type = type;
+	private Checks(String s, WarningType type) {
+	    this((line) -> line.contains(s), type);      
 	}
 
-	private Checks(Pattern p, WarningType type){
-	    this.p = p;
+	private Checks(Pattern p, WarningType type) {
+	    this((line) -> p.matcher(line).find(), type);
+	}
+
+	private Checks(Predicate<String> predicate, WarningType type) {
+	    this.predicate = predicate;
 	    this.type = type;
 	}
 
 	@Override
-	public String toString(){
+	public String toString() {
 	    return super.toString().charAt(0) + super.toString().substring(1).replace("_", "").toLowerCase();
 	}
 
-	public boolean isPattern(){
-	    return (p != null);
+	public boolean matches(String input) {
+	    return predicate.test(input);
 	}
 
-	public String getString(){
-	    return s;
-	}
-
-	public Pattern getPattern(){
-	    return p;
-	}
-
-	public WarningType getType(){
+	public WarningType getType() {
 	    return type;
 	}
     }
